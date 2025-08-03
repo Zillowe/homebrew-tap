28cfbad7679b6d603921e02d528039c65d895b43
Zillowe CI/CD <ci@zillowe.rf.gd>
2025-08-03 13:33:54 +0000

    Release: Bump package version to 3.3.0-beta

--- zoi.rb
+++ zoi.rb
@@ -3,30 +3,30 @@
 class Zoi < Formula
   desc "Universal Package Manager & Environment Setup Tool"
   homepage "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi"
-  version "3.2.7-beta" 
-  _tag = "Prod-Beta-3.2.7"
+  version "3.3.0-beta" 
+  _tag = "Prod-Beta-3.3.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-arm64.tar.zst"
-      sha512 "a7cfeff099ce7ddf667ec97624eda0f2cef7cb602705f07720b4b9834c836d8de10db08cca178519b72704f5eb4b543cbc3bf3f9c48c943e7fbbb92705767a27"
+      sha256 "da52c18722f58d577e035067a1a07fe56f7e3cccabb12cf605ec7614acd86919"
     end
 
     if Hardware::CPU.intel?
       url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-macos-amd64.tar.zst"
-      sha512 "0a0d98670f162561be0005cb244dc5fa63cef188e2c2835c7b2b2b6a4a4da662319c80f38a22e083f4febf2f8d3fdef05a8adf25e80e15b0b3f65a5e9ff36623"
+      sha256 "b207093148d746fdb887c4aa638507bbe49850ce674f23af301d8cf9865c8084"
     end
 
     on_linux do
       if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-amd64.tar.zst"
-        sha512 "80893110edaef73b80d06d98e076e24a8022134e13256b5d452c5a3b36d5154541fbabdeab4a59cb47e64a74ea4e84e4acf24bdeaf7f89e47e77063bdadcdd93"
+        sha256 "0d3456b26576469b6859ed531bad1fe78fbc8edd078fc174ec1126350925debb"
       end
 
       if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
         url "https://gitlab.com/Zillowe/Zillwen/Zusty/Zoi/-/releases/#{_tag}/downloads/zoi-linux-arm64.tar.zst"
-        sha512 "a023b4a3c74ef7a651b441e95b6031f4ada91e9e0cd70cc06aa9df0583ee6531b6848c5cefb465ebf1c37a9ec2796902dc77a147a6cb9215857c93a4162111be"
+        sha256 "d853ae0106d2467d281d17026f1fa57db4f03afdf656810f0925953eace73e39"
       end
     end
 
